ace88d348f19a5c13d02d3dba0e3a45fd19a7276
Muhammad Abdullah <neymark195@gmail.com>
2023-07-24 18:18:56 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const Btn = document.querySelector(".Btn");
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+document.querySelector(".Btn").onclick = addTask;
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+    // Call saveData() to save the updated list to localStorage
+    saveData();
+  }
+
+  inputBox.value = "";
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      // Call saveData() to save the updated list to localStorage
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      // Call saveData() to save the updated list to localStorage
+      saveData();
+    }
+  },
+  false
+);
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+window.addEventListener("load", showTask);
